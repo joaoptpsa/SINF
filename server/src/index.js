e468c80940ba618786d7e32b664c40ed3a9566ce
João Sá <up201506252@fe.up.pt>
2018-10-19 16:13:07 +0100

    Created (working) sketch of db connection :stars:

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,9 @@
-console.log('teste')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://sinf:fakem6@ds155509.mlab.com:55509/sinf');
+
+//Get the default connection
+const db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
