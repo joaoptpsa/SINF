03ecc01c65fc21ddbb95c2444db5fcc6acf8c037
Mateus <mpcmarques@icloud.com>
2018-11-15 16:21:16 +0000

    almost finished parser

--- client/src/models/address.js
+++ client/src/models/address.js
@@ -1,14 +1,12 @@
 class Address {
-    detail;
-    city;
-    country;
-    postalCode;
-
     constructor(XMLElement){
         this.detail = XMLElement.AddressDetail[0];
         this.city = XMLElement.City[0];
         this.country = XMLElement.Country[0];
         this.postalCode = XMLElement.PostalCode[0];
+        if (XMLElement.BuildingNumber) this.buildingNumber = XMLElement.BuildingNumber[0];
+        if (XMLElement.StreetName) this.streetName = XMLElement.StreetName[0];
+        if (XMLElement.Region) this.region = XMLElement.Region[0];
     }
 }
 
--- client/src/models/sourceDocuments.js
+++ client/src/models/sourceDocuments.js
@@ -1,3 +1,87 @@
+import Address from './address';
+import TaxTableEntry from './masterFiles/taxTableEntry';
+
+class OrderReference {
+    constructor(XMLElement){
+        if (XMLElement.OriginatingON) this.originatingON = XMLElement.OriginatingON[0];
+        if (XMLElement.OrderDate) this.orderDate = XMLElement.OrderDate[0];
+    }
+}
+
+class Reference {
+    constructor(XMLElement){
+
+    }
+}
+
+class Line {
+    constructor(XMLElement){
+        this.lineNumber = XMLElement.LineNumber[0];
+        this.orderReferences = [];
+        // Parse order references
+        for(const index in XMLElement.OrderReferences){
+            this.orderReferences.push(new OrderReference(XMLElement.OrderReferences[index]))
+        }
+        
+        this.productCode = XMLElement.ProductCode[0];
+        this.productDescription = XMLElement.ProductDescription[0];
+        this.quantity = XMLElement.Quantity[0];
+        if (XMLElement.UnitOfMeasure) this.unitOfMeasure = XMLElement.UnitOfMeasure[0];
+        this.unitPrice = XMLElement.UnitPrice[0];
+        this.taxPointDate = XMLElement.TaxPointDate[0];
+        
+        // TODO: Parse References
+        
+        this.description = XMLElement.Description[0];
+        this.debitAmount = XMLElement.DebitAmount[0];
+        this.creditAmount = XMLElement.CreditAmount[0];
+        
+        // TODO: Parse tax
+        this.taxes = []
+        for (const index in XMLElement.Tax){
+            this.taxes.push(new TaxTableEntry(XMLElement.Tax[index]));
+        }
+        
+        if (XMLElement.TaxExemptionReason) this.taxExemptionReason = XMLElement.TaxExemptionReason[0];
+        if (XMLElement.SettlementAmount) this.settlementAmount = XMLElement.SettlementAmount[0];
+    }
+}
+
+class Shipment {
+    contructor(XMLElement){
+        if (XMLElement.DeliveryID) this.deliveryID = XMLElement.DeliveryID[0];
+        if (XMLElement.DeliveryDate) this.deliveryDate = XMLElement.DeliveryDate[0];
+        this.address = new Address(XMLElement.Address[0]);
+    }
+}
+
+class Currency {
+    constructor({CurrencyCode, CurrencyDebitAmount, CurrencyCreditAmount}){
+        if (CurrencyCode) this.code = CurrencyCode[0];
+        if (CurrencyDebitAmount) this.debitAmount = CurrencyDebitAmount[0];
+        if (CurrencyCreditAmount) this.creditAmount = CurrencyCreditAmount[0];
+    }
+}
+
+class Settlement {
+    constructor({SettlementDiscount, SettlementAmount, SettlementDate, PaymentMechanism}){
+        if (SettlementDiscount) this.discount = SettlementDiscount[0];
+        if (SettlementAmount) this.amount = SettlementAmount[0];
+        if (SettlementDate) this.date = SettlementDate[0];
+        if (PaymentMechanism) this.mechanism = PaymentMechanism[0];
+    }
+}
+
+class DocumentTotal {
+    constructor(XMLElement){
+        this.taxPayable = XMLElement.TaxPayable[0];
+        this.netTotal = XMLElement.NetTotal[0];
+        this.grossTotal = XMLElement.GrossTotal[0];
+        if (XMLElement.Currency) this.currency = new Currency(XMLElement.Currency[0]);
+        if (XMLElement.Settlement) this.settlement = new Settlement(XMLElement.Settlement[0]);
+    }
+}
+
 class Invoice {
     number;
     status;
@@ -10,10 +94,8 @@ class Invoice {
     systemEntryDate;
     transactionID;
     customerID;
-
+    
     constructor(XMLElement){
-
-
         this.number = XMLElement.InvoiceNo[0];
         if  (XMLElement.InvoiceStatus) this.status = XMLElement.InvoiceStatus[0];
         if (XMLElement.Hash) this.hash = XMLElement.Hash[0];
@@ -25,12 +107,20 @@ class Invoice {
         this.systemEntryDate = XMLElement.SystemEntryDate[0];
         if (XMLElement.TransactionID) this.transactionID = XMLElement.TransactionID[0];
         this.customerID = XMLElement.CustomerID[0];
+        
+        if (XMLElement.ShipTo) this.shipTo = new Shipment(XMLElement.ShipTo[0]);
+        if (XMLElement.ShipFrom) this.shipFrom = new Shipment(XMLElement.ShipFrom[0]);
 
-        // TODO: 
-        console.log(XMLElement.ShipTo);
-        console.log(XMLElement.ShipFrom);
-        console.log(XMLElement.Line);
-        console.log(XMLElement.DocumentTotals);
+        // parse lines
+        this.lines = []
+        for (const index in XMLElement.Line){
+            this.lines.push(new Line(XMLElement.Line[index]));
+        }
+
+        this.documentTotals = [];
+        for(const index in XMLElement.DocumentTotals){
+            this.documentTotals.push(new DocumentTotal(XMLElement.DocumentTotals[index]));
+        }
     }
 }
 
@@ -39,12 +129,12 @@ class SalesInvoices {
     totalDebit;
     totalCredit;
     invoices;
-
+    
     constructor(XMLElement){
         this.numberOfEntries = XMLElement.NumberOfEntries[0];
         this.totalDebit = XMLElement.TotalDebit[0];
         this.totalCredit = XMLElement.TotalCredit[0];
-
+        
         this.invoices = [];
         for (const index in XMLElement.Invoice){
             this.invoices.push(new Invoice(XMLElement.Invoice[index]));
@@ -55,5 +145,7 @@ class SalesInvoices {
 export default (XMLElement) => {
     const salesInvoices = new SalesInvoices(XMLElement.SalesInvoices[0])
 
+    console.log(salesInvoices, XMLElement.SalesInvoices[0])
+    
     return salesInvoices
 }
