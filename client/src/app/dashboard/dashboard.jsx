445557d63380532e47907401299dfdf1be96eb24
Mateus <mpcmarques@icloud.com>
2018-11-18 19:34:31 +0000

    css :art:

--- client/src/app/dashboard/dashboard.jsx
+++ client/src/app/dashboard/dashboard.jsx
@@ -9,26 +9,24 @@ const menuItemStyle = {
 };
 
 const Dashboard = () => (
-  <Container fluid>
-    <Grid columns={2}>
-      <Grid.Row>
-        <Grid.Column width={2} style={{ backgroundColor: '#504249', minHeight: '100%', padding: '33px' }}>
-          <Header style={{ color: 'white' }}>
-            <Icon name="dashboard" />
+  <Container fluid style={{ height: '100%' }}>
+    <Grid columns={2} style={{ minHeight: '100%' }}>
+      <Grid.Column width={2} style={{ backgroundColor: '#504249', minHeight: '100%', padding: '33px' }}>
+        <Header style={{ color: 'white' }}>
+          <Icon name="dashboard" />
               360 Company Dashboard
-          </Header>
-          <Menu text vertical>
-            <Menu.Item name="Overview" style={menuItemStyle} />
-            <Menu.Item name="Sales" style={menuItemStyle} />
-            <Menu.Item name="Purchases" style={menuItemStyle} />
-            <Menu.Item name="Inventory" style={menuItemStyle} />
-            <Menu.Item name="Finances" style={menuItemStyle} />
-          </Menu>
-        </Grid.Column>
-        <Grid.Column width={14} style={{ backgroundColor: '#D8D8D8', padding: '20px' }}>
-          <Overview />
-        </Grid.Column>
-      </Grid.Row>
+        </Header>
+        <Menu text vertical>
+          <Menu.Item name="Overview" style={menuItemStyle} />
+          <Menu.Item name="Sales" style={menuItemStyle} />
+          <Menu.Item name="Purchases" style={menuItemStyle} />
+          <Menu.Item name="Inventory" style={menuItemStyle} />
+          <Menu.Item name="Finances" style={menuItemStyle} />
+        </Menu>
+      </Grid.Column>
+      <Grid.Column width={14} style={{ backgroundColor: '#D8D8D8', padding: '20px' }}>
+        <Overview />
+      </Grid.Column>
     </Grid>
   </Container>
 );
--- client/src/app/dashboard/overview.jsx
+++ client/src/app/dashboard/overview.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Divider, Select, Grid, Segment, Header,
+  Divider, Select, Grid, Segment, Header, Icon,
 } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
@@ -15,16 +15,28 @@ const selectStyle = {
   color: 'black',
 };
 
-const GrowthSegment = ({ text, number }) => (
-  <Segment textAlign="center">
+const GrowthSegment = ({ text, number, isNegative }) => (
+  <Segment textAlign="center" style={{ margin: '16px' }}>
     <Header>{text}</Header>
-    {number}
+    <Grid columns={2}>
+      <Grid.Column>
+        <Header as="h2" style={{ paddingTop: '5px' }}>{number}</Header>
+      </Grid.Column>
+      <Grid.Column>
+        {isNegative ? <Icon size="big" color="red" name="arrow circle down" /> : <Icon size="big" color="green" name="arrow circle up" />}
+      </Grid.Column>
+    </Grid>
   </Segment>
 );
+
+GrowthSegment.defaultProps = {
+  isNegative: false,
+};
 
 GrowthSegment.propTypes = {
   text: PropTypes.string.isRequired,
   number: PropTypes.string.isRequired,
+  isNegative: PropTypes.bool,
 };
 
 const Overview = () => (
@@ -34,19 +46,19 @@ const Overview = () => (
     <Grid>
       <Grid.Row columns={5}>
         <Grid.Column>
-          <GrowthSegment text="Liquidity" number="100%" />
+          <GrowthSegment text="Liquidity" number="100%" isNegative />
         </Grid.Column>
         <Grid.Column>
           <GrowthSegment text="Total Sales" number="100%" />
         </Grid.Column>
         <Grid.Column>
-          <GrowthSegment text="Total Purchases" number="100%" />
+          <GrowthSegment text="Total Purchases" number="100%" isNegative />
         </Grid.Column>
         <Grid.Column>
           <GrowthSegment text="Gross Profit" number="100%" />
         </Grid.Column>
         <Grid.Column>
-          <GrowthSegment text="Total Stock Value" number="100%" />
+          <GrowthSegment text="Total Stock Value" number="100%" isNegative />
         </Grid.Column>
       </Grid.Row>
     </Grid>
