d749a412b3993acec38857c7f607690b8db502c9
Mateus <mpcmarques@icloud.com>
2018-11-20 17:26:53 +0000

    finished calculating best seller products

--- client/src/app/dashboard/overview/index.jsx
+++ client/src/app/dashboard/overview/index.jsx
@@ -12,6 +12,7 @@ import {
   YAxis,
   PieChart,
   Pie,
+  Cell,
 } from 'recharts';
 import PropTypes from 'prop-types';
 import GrowthSegment from './growthSegment';
@@ -57,74 +58,119 @@ const data = [
   },
 ];
 
-const pieData = [
-  { name: 'Cellphones', value: 30 },
-  { name: 'PCs', value: 20 },
-  { name: 'Hardware', value: 15 },
-  { name: 'Others', value: 5 },
-];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#FF4444'];
 
 // TODO: Show SAFT data
 
-const Overview = ({ SAFT }) => (
-  <Segment>
-    <Select placeholder="Select time" options={options} style={selectStyle} />
-    <Divider />
-    <Grid stackable>
-      <Grid.Row columns={5}>
-        <Grid.Column>
-          <GrowthSegment text="Liquidity" number="100%" isNegative />
-        </Grid.Column>
-        <Grid.Column>
-          <GrowthSegment text="Total Sales" number="100%" />
-        </Grid.Column>
-        <Grid.Column>
-          <GrowthSegment text="Total Purchases" number="100%" isNegative />
-        </Grid.Column>
-        <Grid.Column>
-          <GrowthSegment text="Gross Profit" number="100%" />
-        </Grid.Column>
-        <Grid.Column>
-          <GrowthSegment text="Total Stock Value" number="100%" isNegative />
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row columns={2}>
-        <Grid.Column>
-          <Segment compact>
-            <Header>Most valuable costumers</Header>
-            <BarChart
-              width={600}
-              height={300}
-              data={data}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="value" fill="#8884d8" />
-            </BarChart>
-          </Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment compact>
-            <Header>Best seller product</Header>
-            <PieChart width={400} height={300}>
-              <Pie data={pieData} dataKey="value" label />
-              <Legend />
-            </PieChart>
-          </Segment>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </Segment>
-);
+const getSourceDocumentLines = (invoices) => {
+  const sourceDocumentLines = [];
+
+  invoices.forEach((invoice) => {
+    invoice.lines.forEach((line) => {
+      sourceDocumentLines.push(line);
+    });
+  });
+
+  return sourceDocumentLines;
+};
+
+const getSoldProducts = (invoices) => {
+  const products = {};
+
+  getSourceDocumentLines(invoices).forEach((line) => {
+    if (products[line.productCode]) products[line.productCode] += parseFloat(line.quantity);
+    else products[line.productCode] = parseFloat(line.quantity);
+  });
+
+  return products;
+};
+
+const getTop5SoldProducts = (invoices) => {
+  // get all sold products
+  const products = getSoldProducts(invoices);
+
+  // get codes of the top 5 most sold products
+  const sortedKeys = Object.keys(products)
+    .sort((a, b) => products[b] - products[a])
+    .splice(0, 4);
+
+  const top5products = [];
+  sortedKeys.forEach((key) => {
+    top5products.push({ code: key, quantity: products[key] });
+  });
+
+  return top5products;
+};
+
+const Overview = ({ SAFT }) => {
+  const top5Products = getTop5SoldProducts(SAFT.sourceDocuments.invoices);
+
+  console.log(top5Products);
+
+  return (
+    <Segment>
+      <Select placeholder="Select time" options={options} style={selectStyle} />
+      <Divider />
+      <Grid stackable>
+        <Grid.Row columns={5}>
+          <Grid.Column>
+            <GrowthSegment text="Liquidity" number="100%" isNegative />
+          </Grid.Column>
+          <Grid.Column>
+            <GrowthSegment text="Total Sales" number="100%" />
+          </Grid.Column>
+          <Grid.Column>
+            <GrowthSegment text="Total Purchases" number="100%" isNegative />
+          </Grid.Column>
+          <Grid.Column>
+            <GrowthSegment text="Gross Profit" number="100%" />
+          </Grid.Column>
+          <Grid.Column>
+            <GrowthSegment text="Total Stock Value" number="100%" isNegative />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <Segment compact>
+              <Header>Most valuable costumers</Header>
+              <BarChart
+                width={600}
+                height={300}
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="value" fill="#8884d8" />
+              </BarChart>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment compact>
+              <Header>Best seller product</Header>
+              <PieChart width={400} height={300}>
+                <Pie data={top5Products} dataKey="quantity" nameKey="code" label>
+                  {data.map((entry, index) => (
+                    <Cell key="index" fill={COLORS[index]} />
+                  ))}
+                </Pie>
+                <Legend />
+              </PieChart>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+  );
+};
 
 Overview.propTypes = {
   SAFT: PropTypes.object.isRequired,
