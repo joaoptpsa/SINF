0558480f4c23d57b95ac4eba62e03272b37ac032
Mateus <mpcmarques@icloud.com>
2018-11-23 18:08:12 +0000

    created link to pages

--- client/src/app/dashboard/index.jsx
+++ client/src/app/dashboard/index.jsx
@@ -3,6 +3,7 @@ import {
   Grid, Menu, Container, Header, Icon,
 } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Overview from './overview';
 
 const menuItemStyle = {
@@ -11,30 +12,42 @@ const menuItemStyle = {
 
 const Dashboard = ({ SAFT }) => (
   <Container fluid style={{ height: '100%' }}>
-    <Grid columns={2} style={{ minHeight: '100%' }} stackable>
-      <Grid.Column
-        width={2}
-        style={{ backgroundColor: '#504249', minHeight: '100%', padding: '33px' }}
-      >
-        <Header style={{ color: 'white' }}>
-          <Icon name="dashboard" />
-          360 Company Dashboard
-        </Header>
-        <Menu text vertical>
-          <Menu.Item name="Overview" style={menuItemStyle} />
-          <Menu.Item name="Sales" style={menuItemStyle} />
-          <Menu.Item name="Purchases" style={menuItemStyle} />
-          <Menu.Item name="Inventory" style={menuItemStyle} />
-          <Menu.Item name="Finances" style={menuItemStyle} />
-        </Menu>
-      </Grid.Column>
-      <Grid.Column
-        width={14}
-        style={{ backgroundColor: '#D8D8D8', paddingTop: '30px', paddingRight: '30px' }}
-      >
-        <Overview SAFT={SAFT} />
-      </Grid.Column>
-    </Grid>
+    <Router>
+      <Grid columns={2} style={{ minHeight: '100%' }} stackable>
+        <Grid.Column
+          width={2}
+          style={{ backgroundColor: '#504249', minHeight: '100%', padding: '33px' }}
+        >
+          <Header style={{ color: 'white' }}>
+            <Icon name="dashboard" />
+            360 Company Dashboard
+          </Header>
+          <Menu text vertical>
+            <Menu.Item name="Overview" style={menuItemStyle}>
+              <Link to="/">Overview</Link>
+            </Menu.Item>
+            <Menu.Item name="Sales" style={menuItemStyle}>
+              <Link to="/sales/">Sales</Link>
+            </Menu.Item>
+            <Menu.Item name="Purchases" style={menuItemStyle} />
+            <Menu.Item name="Inventory" style={menuItemStyle} />
+            <Menu.Item name="Finances" style={menuItemStyle}>
+              <Link to="/finances/">Finances</Link>
+            </Menu.Item>
+          </Menu>
+        </Grid.Column>
+        <Grid.Column
+          width={14}
+          style={{ backgroundColor: '#D8D8D8', paddingTop: '30px', paddingRight: '30px' }}
+        >
+          {/* Overview page */}
+          <Route path="/" exact render={props => <Overview SAFT={SAFT} {...props} />} />
+          {/* Sales page */}
+          <Route path="/sales/" component={null} />
+          {/* */}
+        </Grid.Column>
+      </Grid>
+    </Router>
   </Container>
 );
 
