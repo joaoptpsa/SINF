e3fb13c3ee0c168b3c2803d13ae79ed8068f7eca
Mateus <mpcmarques@icloud.com>
2018-11-23 18:29:30 +0000

    added server

---
+++ client/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Started server at ${port}!`);
+});
