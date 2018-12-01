1c5745a644cc4e1f696cbce563bb42522c2c8115
João Sá <up201506252@fe.up.pt>
2018-12-01 18:19:57 +0000

    purchases :gem:

--- web/src/app/dashboard/purchases/index.jsx
+++ web/src/app/dashboard/purchases/index.jsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 import dashboardPage from '../dashboardPage';
+import GrowthSegment from '../growthSegment';
+import MostValuableCostumersSegment from '../mostValuableCostumersSegment';
+import TopProductsPiechartSegment from '../topProductsPiechartSegment';
+
 
 const Purchases = (props) => {
     const {
-        SAFT
+        SAFT,
+        numSales,
+        grossProfit,
+        top5Costumers,
+        top5Products,
+        numCostumers,
+        getNumSales,
+        getNumCostumers,
+        getGrossProfitFromInvoices,
     } = props;
 
     return (
-        /* feel free to delete code below*/
-        < Card fluid >
-            <Card.Content>
-                <Card.Header>
-                    Purchases Page
-                </Card.Header>
-            </Card.Content>
-            <Card.Content>
-                Lorem xpmsasd
-            </Card.Content>
-        </Card >
+        <Grid>
+            <Grid.Row columns={4}>
+                <Grid.Column>
+                    <GrowthSegment text="Total purchases" number={100} />
+                </Grid.Column>
+                <Grid.Column>
+                    <GrowthSegment text="Total suppliers" number={100} />
+                </Grid.Column>
+                <Grid.Column>
+                    <GrowthSegment text="Purchases growth" number={100} />
+                </Grid.Column>
+                <Grid.Column>
+                    <GrowthSegment text="Number of purchases" number={100} />
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={2}>
+                <Grid.Column width={6}>
+                    <TopProductsPiechartSegment title="Top items bought" top5Products={top5Products} />
+                </Grid.Column>
+                <Grid.Column width={10}>
+                    <MostValuableCostumersSegment top5Costumers={top5Costumers} />
+                </Grid.Column>
+            </Grid.Row>
+            <Grid.Row columns={2}>
+                <Grid.Column width={10}>
+                    <MostValuableCostumersSegment top5Costumers={top5Costumers} />
+                </Grid.Column>
+                <Grid.Column width={6}>
+                    <TopProductsPiechartSegment title="Spending by item" top5Products={top5Products} />
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
     );
 };
 
