a1df7676aea6d58c2b1580c30e049131da54df46
Mateus <mateus@MacBook-Pro-de-Mateus.local>
2018-12-14 11:12:06 +0000

    file input loading state

--- web/src/app/fileInput.jsx
+++ web/src/app/fileInput.jsx
@@ -1,41 +1,57 @@
 import * as React from 'react';
 import {
-  Container, Header, Icon, Input,
+  Header, Icon, Input, Segment,
 } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
-const FileInput = ({ handleFile }) => {
-  const handleChange = (e) => {
+class FileInput extends React.Component {
+  state = { loading: false };
+
+  handleChange = (e) => {
+    const { handleFile } = this.props;
     const reader = new FileReader();
 
     reader.onload = () => {
+      this.setState({ loading: false });
+
       const text = reader.result;
 
       handleFile(text);
     };
 
+    reader.onerror = (err) => {
+      this.setState({ loading: false });
+      alert(err);
+    };
+
+    this.setState({ loading: true });
     reader.readAsText(e.target.files[0], 'windows-1252');
   };
 
-  return (
-    <Container
-      textAlign="center"
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        itemsAlign: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Header as="h2" icon>
-        <Icon name="cloud upload" />
-        Upload SAF-T
-      </Header>
+  render() {
+    const { loading } = this.state;
 
-      <Input type="file" accept=".xml" onChange={handleChange} />
-    </Container>
-  );
-};
+    return (
+      <Segment
+        textAlign="center"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          itemsAlign: 'center',
+          justifyContent: 'center',
+        }}
+        loading={loading}
+      >
+        <Header as="h2" icon>
+          <Icon name="cloud upload" />
+          Upload SAF-T
+        </Header>
+
+        <Input type="file" accept=".xml" onChange={this.handleChange} />
+      </Segment>
+    );
+  }
+}
 
 FileInput.propTypes = {
   handleFile: PropTypes.func.isRequired,
