6ba408d40b7372123e765f7ea8b05324ea3979e9
João Sá <up201506252@fe.up.pt>
2018-12-16 21:55:27 +0000

    :bug:

--- web/src/app/dashboard/finances/index.jsx
+++ web/src/app/dashboard/finances/index.jsx
@@ -5,6 +5,10 @@ import PropTypes from 'prop-types';
 import DisplaySegment from '../displaySegment';
 
 const getGeneralLedgerAccount = (SAFT, id) => {
+  if (!SAFT.masterFiles.generalLedgerAccounts[`${id}`]) {
+    return null;
+  }
+
   const Account = {};
   Account.openingPeriod = Number.parseFloat(SAFT.masterFiles.generalLedgerAccounts[`${id}`].openingDebitBalance)
     - Number.parseFloat(SAFT.masterFiles.generalLedgerAccounts[`${id}`].openingCreditBalance);
@@ -22,15 +26,18 @@ const getGrossProfit = (SAFT) => {
   const netRevenue = getGeneralLedgerAccount(SAFT, 71);
   const cogs = getGeneralLedgerAccount(SAFT, 61);
 
-  const gp = {};
-  gp.openingPeriod = netRevenue.openingPeriod - cogs.openingPeriod;
-  gp.closingPeriod = netRevenue.closingPeriod - cogs.closingPeriod;
-  let openingPeriodSign = 1;
-  if (gp.openingPeriod < 0) {
-    openingPeriodSign *= -1;
+  if (netRevenue && cogs) {
+    const gp = {};
+    gp.openingPeriod = netRevenue.openingPeriod - cogs.openingPeriod;
+    gp.closingPeriod = netRevenue.closingPeriod - cogs.closingPeriod;
+    let openingPeriodSign = 1;
+    if (gp.openingPeriod < 0) {
+      openingPeriodSign *= -1;
+    }
+    gp.growth = (gp.closingPeriod / gp.openingPeriod) * 100 * openingPeriodSign;
+    return gp;
   }
-  gp.growth = (gp.closingPeriod / gp.openingPeriod) * 100 * openingPeriodSign;
-  return gp;
+  return null;
 };
 
 const getQuickRatio = (SAFT) => {
@@ -50,10 +57,44 @@ const Finances = (props) => {
   const grossProfit = getGrossProfit(SAFT);
   const quickRatio = getQuickRatio(SAFT);
 
+  if (grossProfit) {
+    return (
+      <Segment>
+        <Grid>
+          <Grid.Row columns={3}>
+            <Grid.Column>
+              <DisplaySegment
+                text="Accounts Receivable"
+                number={ar.closingPeriod}
+                type="€"
+                growth={ar.growth}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <DisplaySegment
+                text="Accounts Payable"
+                number={ap.closingPeriod}
+                type="€"
+                growth={ap.growth}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <DisplaySegment
+                text="Gross Profit"
+                number={grossProfit.closingPeriod}
+                type="€"
+                growth={grossProfit.growth}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+    );
+  }
   return (
     <Segment>
       <Grid>
-        <Grid.Row columns={3}>
+        <Grid.Row columns={2}>
           <Grid.Column>
             <DisplaySegment
               text="Accounts Receivable"
@@ -68,14 +109,6 @@ const Finances = (props) => {
               number={ap.closingPeriod}
               type="€"
               growth={ap.growth}
-            />
-          </Grid.Column>
-          <Grid.Column>
-            <DisplaySegment
-              text="Gross Profit"
-              number={grossProfit.closingPeriod}
-              type="€"
-              growth={grossProfit.growth}
             />
           </Grid.Column>
         </Grid.Row>
